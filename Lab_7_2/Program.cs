3d62bd2c40378d460dddff9c72302935b1b367a5
11111 <11111@DESKTOP-I2P1BG6>
2021-11-23 01:38:49 +0300

    Add project files.

---
+++ Lab_7_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            double S, V;
+            GetParam(a, out S, out V);
+            Console.WriteLine("S={0}", S);
+            Console.WriteLine("V={0}", V);
+            Console.ReadKey();
+        }
+        static void GetParam(int a, out double S, out double V)
+        {
+            S = 6*a*a;
+            V = a*a*a;
+        }
+    }
+}
